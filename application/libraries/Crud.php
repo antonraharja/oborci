515a571ddc1a6253ae0e9ca70e5b498d2508045c
Anton Raharja <antonrd@gmail.com>
2011-05-10 11:41:09 +0700

    add CRUD library

---
+++ application/libraries/Crud.php
@@ -0,0 +1,47 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * CRUD library
+ * @author Anton Raharja
+ *
+ */
+class Crud {
+
+	/**
+	 * Enter description here ...
+	 * $this->crud->set_name('roles');
+	 * @param string $name
+	 */
+	public function set_name($name) {
+
+	}
+
+	/**
+	 * Enter description here ...
+	 * $this->crud->set_grid($data);
+	 * @param array $data
+	 */
+	public function set_grid($data) {
+			
+	}
+
+	/**
+	 * Enter description here ...
+	 * $this->crud->set_uri('index');
+	 * @param unknown_type $uri
+	 */
+	public function set_uri($uri) {
+
+	}
+
+	/**
+	 * Enter description here ...
+	 * return $this->crud->render();
+	 */
+	public function render() {
+		return 'CRUD';
+	}
+
+}
+
+/* End of file SC_CRUD.php */
