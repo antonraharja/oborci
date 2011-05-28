a2c9b25ae7494c3ff2ac1ccec0b35bd8c5864e9a
Anton Raharja <antonrd@gmail.com>
2011-05-28 16:21:20 +0700

    fix translate_helper.php, add ability to use language file, not finished yet

--- application/helpers/oborci/translate_helper.php
+++ application/helpers/oborci/translate_helper.php
@@ -8,30 +8,42 @@
 /**
  * Translate string
  * @param string $str String to be translated
+ * @param string $lang_file Language file, with or without extension
  * @return string Translated string
  */
-function translate($str) {
-        $returns = NULL;
-	if (function_exists('_')) {
-                // in case php gettext is installed
-		$returns = _($str);
-	} else {
-                // your translation codes here
-                $lang['Test'] = 'Test';
-                $lang['Coba'] = 'Test';
-                $lang['Uji'] = 'Test';
-		$returns = isset($lang[$str]) ? $lang[$str] : $str;
-	}
+function oborci_translate($str, $lang_file=NULL) {
+        global $application_folder;
+        if (isset($lang_file)) {
+                $lang_file = str_replace('..', '.', $lang_file);
+                $lang_file = str_replace('|', '', $lang_file);
+                $lang_file = str_replace('`', '', $lang_file);
+                $lang_file = str_replace('\'', '', $lang_file);
+                $lang_file = str_replace('"', '', $lang_file);
+                $lang_file = $application_folder.'/language/'.$lang_file.'_lang.php';
+                if (file_exists($lang_file)) {
+                        // include a language file $lang_file
+                        // the language file contains $lang array of text and its translation
+                        // the language file used is the same language file supported by CI
+                        @include_once($lang_file);
+                }
+        } else {
+                $lang_file = $application_folder.'/language/oborci_lang.php';
+                if (file_exists($lang_file)) {
+                        @include_once($lang_file);
+                }
+        }
+        $returns = isset($lang[$str]) ? $lang[$str] : $str;
 	return $returns;
 }
 
 /**
  * Shortify translate() to t()
  * @param string $str String to be translated
+ * @param string $lang_file Language file, with or without extension
  * @return string Translated string
  */
-function t($str) {
-        return translate($str);
+function t($str, $lang_file=NULL) {
+        return oborci_translate($str, $lang_file);
 }
 
 ?>
