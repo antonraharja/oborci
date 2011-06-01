b65d5a21313fab6bb6a89e99c2b6164a2112282a
Anton Raharja <antonrd@gmail.com>
2011-06-02 05:26:55 +0700

    more extjs codes on example2, demonstrates login and home

--- oborci-source/application/views/example2/login_view.php
+++ oborci-source/application/views/example2/login_view.php
@@ -3,7 +3,7 @@
         <head>
                 <title>Oborci Example2</title>
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/example2/css/ext/ext-all.css" />
-                <script type="text/javascript" src="<?php echo base_url(); ?>assets/example2/js/ext/ext-all.js"></script>
+                <script type="text/javascript" src="<?php echo base_url(); ?>assets/example2/js/ext/ext-all-debug.js"></script>
                 <script type="text/javascript" src="<?php echo base_url(); ?>assets/example2/app/Oborci.login/login.js"></script>
         </head>
         <body></body>
--- oborci-source/assets/example2/app.js
+++ oborci-source/assets/example2/app.js
@@ -1,6 +1,95 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+Ext.onReady(function(){
+        var MyWindowUi = Ext.extend(Ext.Window, {
+                title: 'Oborci Example2',
+                width: '100%',
+                height: '100%',
+                closable: false,
+                draggable: false,
+                resizable: false,
+                layout: 'fit',
+                initComponent: function() {
+                        this.tbar = {
+                                xtype: 'toolbar',
+                                items: [
+                                {
+                                        xtype: 'button',
+                                        text: 'Home'
+                                },
+                                {
+                                        xtype: 'button',
+                                        text: 'Master',
+                                        menu: {
+                                                xtype: 'menu',
+                                                items: [
+                                                {
+                                                        xtype: 'menuitem',
+                                                        text: 'Role Management',
+                                                        handler: onMenuClick
+                                                },
+                                                {
+                                                        xtype: 'menuitem',
+                                                        text: 'User Management',
+                                                        handler: onMenuClick
+                                                },
+                                                {
+                                                        xtype: 'menuitem',
+                                                        text: 'Screen Management',
+                                                        handler: onMenuClick
+                                                },
+                                                {
+                                                        xtype: 'menuitem',
+                                                        text: 'Menu Management',
+                                                        handler: onMenuClick
+                                                }
+                                                ]
+                                        }
+                                },
+                                {
+                                        xtype: 'button',
+                                        text: 'Logout',
+                                        handler: onLogoutClick
+                                }
+                                ]
+                        };
+                        this.items = [
+                        {
+                                xtype: 'treepanel',
+                                title: 'My Tree',
+                                width: 260,
+                                height: 511,
+                                root: {
+                                        text: 'Tree Node'
+                                },
+                                loader: {
 
+                        }
+                        }
+                        ];
+                        MyWindowUi.superclass.initComponent.call(this);
+                }
+        });        
+        
+        var cmp = new MyWindowUi();
+        cmp.show();
+
+        function onMenuClick(item){
+                Ext.Msg.alert(item.text);
+        }
+        
+        function onLogoutClick() {
+                Ext.Msg.show({
+                        title: 'Logout',
+                        msg: 'Do you really want to logout ?',
+                        buttons: Ext.Msg.YESNO,
+                        fn: processLogout,
+                        icon: Ext.window.MessageBox.QUESTION
+                });
+        }
+        
+        function processLogout(btn) {
+                if (btn=='yes') {
+                        window.location.href = 'process/logout';
+                }
+        }
+});
 
